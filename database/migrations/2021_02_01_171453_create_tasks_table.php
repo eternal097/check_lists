cc6450c361931f2e267953e3cbef2b66aa3d0316
igor_karachev <igor73rus13@gmail.com>
2021-02-01 22:05:17 +0400

    add migrations

---
+++ database/migrations/2021_02_01_171453_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('task_id');
+            $table->bigInteger('checklist_id');
+            $table->foreign('checklist_id')
+                ->references('id')->on('checklists')
+                ->onDelete('cascade');
+            $table->string('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
