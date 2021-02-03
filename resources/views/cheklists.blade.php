cb210032261be3522962bca55ee28dccaa224f4d
Ilia Merlushkin <nerzyl94@gmail.com>
2021-02-03 10:18:48 +0400

    add validation

--- resources/views/cheklists.blade.php
+++ resources/views/cheklists.blade.php
@@ -7,6 +7,15 @@
             <div class="card">
                 <div class="card-header">Your checklists</div>
                 <div class="card-body">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     <form action="{{route('checklist.store')}}" method="POST">
                         @csrf
                         <div class="input-group mb-3 w-100">
