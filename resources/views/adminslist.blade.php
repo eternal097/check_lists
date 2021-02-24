60a50b95fa967ceb70900d252cb1427aafb2c46a
Karachev Igor <karachevigor99@gmail.com>
2021-02-24 09:02:00 +0400

    add readme

--- resources/views/adminslist.blade.php
+++ resources/views/adminslist.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Users list</div>
+                <div class="card-header">Admins list</div>
                 <div class="card-body">
                   @if(count($admins) != 0)
                   <table class="table">
